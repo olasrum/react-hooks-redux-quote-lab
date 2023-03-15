57f50ad100e556fd2d382ed39256782b7ffce0fb
olasrum <mursalo@gmail.com>
2023-03-15 13:21:24 +1000

    Done with assignment

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,71 @@
 // Action Creators
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: "quotes/add",
+    payload: quote,
+  };
+};
+
+export const removeQuote = (id) => {
+  return {
+    type: "quotes/remove",
+    payload: id,
+  };
+};
+
+export const upvoteQuote = (id) => {
+  return {
+    type: "quotes/upvote",
+    payload: id,
+  };
+};
+
+export const downvoteQuote = (id) => {
+  return {
+    type: "quotes/downvote",
+    payload: id,
+  };
+};
 
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
+  const {type, payload} = action;
+  switch (type) {
+    case "quotes/add":
+      return [...state, payload];
+
+    case "quotes/remove":
+      return state.filter((quote) => quote.id !== payload)
+
+    case "quotes/upvote":
+      return state.map((quote) => {
+        if (quote.id === payload) {
+          return {
+            ...quote,
+            votes: quote.votes + 1,
+          };
+        } else {
+          return quote;
+        }
+      });
+
+    case "quotes/downvote":
+      return state.map((quote) => {
+        if (quote.id === payload && quote.votes > 0) {
+          return {
+            ...quote,
+            votes: quote.votes -1,
+          };
+        } else {
+          return quote;
+        }
+      });
+  
+    default:
+      break;
+  }
   return state;
 }
